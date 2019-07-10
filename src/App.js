c8e2e82d71a0f8c42394c854fe15d11ec229d155
Michael Harris <mhguitar1997@gmail.com>
2019-07-10 12:33:21 -0700

    State add for photo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect}from "react";
 import "./App.css";
 
 function App() {
+
+  const [photo, setPhoto] = useState([])
   return (
     <div className="App">
       <p>
