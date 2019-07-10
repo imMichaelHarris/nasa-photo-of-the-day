61353dbe81852ef481e8043817668c06257d0291
Michael Harris <mhguitar1997@gmail.com>
2019-07-10 12:33:58 -0700

    Get photo add in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, {useState, useEffect}from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-
-  const [photo, setPhoto] = useState([])
+  const [photo, setPhoto] = useState([]);
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        <button>Get Photo</button>
       </p>
     </div>
   );
