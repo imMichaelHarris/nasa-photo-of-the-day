3cb47a55973de52a01fe04d6290b6b4670d5da97
Michael Harris <mhguitar1997@gmail.com>
2019-07-10 12:46:48 -0700

    Use effect to get Nasa data on mount

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios'
+import axios from "axios";
 import "./App.css";
 import Photo from "./Photo";
 
 function App() {
   const [photo, setPhoto] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=fa5O3BQljsEyqfNarlgF5bX4t4TbK6vLbySh9i7H"
+      )
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }, []);
   return (
     <div className="App">
       <p>
