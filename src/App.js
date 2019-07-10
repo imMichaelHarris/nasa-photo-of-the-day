77aaeec77e9a0fc413189262f0f3ef61a6f782bf
Michael Harris <mhguitar1997@gmail.com>
2019-07-10 14:02:12 -0700

    Change handler for hd photo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
-import Media from './Media'
+import Media from "./Media";
 import MediaInfo from "./MediaInfo";
 import HDMedia from "./HDMedia";
 function App() {
   const [media, setMedia] = useState([]);
-  const [hd, setHD] = useState(false)
+  const [hd, setHD] = useState(false);
 
   const getAPOD = () => {
     axios
@@ -20,20 +20,18 @@ function App() {
       .catch(err => console.log(err));
   };
   useEffect(() => getAPOD(), []);
-  console.log(media)
+  console.log(media);
 
-  showHD = () => {
-
-  }
+  const showHD = () => setHD(!hd);
   
+
   return (
     <div className="App">
       <p>
-        <button >Show HD Photo</button>
+        <button>Show HD Photo</button>
         <HDMedia />
-        <MediaInfo title={media.title} date={media.date}/>
-        <Media media={media.url}/>
-
+        <MediaInfo title={media.title} date={media.date} />
+        <Media media={media.url} />
       </p>
     </div>
   );
