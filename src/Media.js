0487a44fe2a731391aad024055c4b7104106d3dc
Michael Harris <mhguitar1997@gmail.com>
2019-07-11 12:34:14 -0700

    IMport Card in Media component

--- src/Media.js
+++ src/Media.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactPlayer from "react-player";
+import { Card } from "semantic-ui-react";
 
 const Media = ({ media, type }) => {
   return (
     <div>
-      {type === "video" ? <ReactPlayer url={media} /> : <img src={media} alt="Nasa Photo of the day"/>}
+      {type === "video" ? (
+        <ReactPlayer url={media} />
+      ) : (
+        <img src={media} alt="Nasa Photo of the day" />
+      )}
     </div>
   );
 };
