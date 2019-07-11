1797b82fdcccb0cc8609cdaee41c32e45678f922
Michael Harris <mhguitar1997@gmail.com>
2019-07-11 12:33:23 -0700

    Import semantic css in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import 'semantic-ui-css/semantic.min.css'
 import "./App.css";
 import Media from "./Media";
 import MediaInfo from "./MediaInfo";
