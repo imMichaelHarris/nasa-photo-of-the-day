5d48e7a8a73b54bea676a54f140938e06bf558a6
Michael Harris <mhguitar1997@gmail.com>
2019-07-11 12:19:04 -0700

    Media renders img tag or react player

--- src/Media.js
+++ src/Media.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactPlayer from 'react-player';
+import React from "react";
+import ReactPlayer from "react-player";
 
-const Media = ({media, type}) => {
-    return (
-        <div>
-            {}<img src={props.media}/>
-        </div>
-    );
+const Media = ({ media, type }) => {
+  return (
+    <div>
+      {type === "video" ? <ReactPlayer url={media} /> : <img src={media} alt="Nasa Photo of the day"/>}
+    </div>
+  );
 };
 
 export default Media;
